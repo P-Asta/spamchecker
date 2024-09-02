f1a8316899a813822fb0b2df507c2a22cd46aa71
5-23 <yhanbyeol6@gmail.com>
2024-09-02 10:28:50 +0900

    3rd try

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use linfa_bayes::{GaussianNb, GaussianNbParams};
 use ndarray::{Array1, Array2};
 use std::io::BufRead;
 
-const MAX_LEN: usize = 20;
+const MAX_LEN: usize = 25;
 
 fn main() {
     let mut x = vec![];
@@ -20,9 +20,6 @@ fn main() {
         let converted = convert(data.message);
         x.push(converted.clone());
         y.push(if data.category == *"ham" { 0 } else { 1 });
-        if data.category == "spam" {
-            println!("{:?}", converted);
-        }
     }
     let flat_x: Vec<f32> = x.iter().flatten().map(|x| *x).collect();
     let x = Array2::from_shape_vec((x.len(), MAX_LEN), flat_x).unwrap();
@@ -33,36 +30,29 @@ fn main() {
     let model: GaussianNb<_, _> = GaussianNb::params().fit(&train).unwrap();
 
     //let input = std::io::stdin().lock().lines().next().unwrap().unwrap();
-    //let input = "if you pick the wrong key your system32 is deleted".to_string();
-    let input = "
-@here
-Dioxus 0.5 is here!
 
-https://www.reddit.com/r/rust/comments/1bpvy1u/dioxus_05_huge_signal_rewrite_remove_lifetimes/
+    //let input = "
+    //18+ Teen Girls and onlyfans leaks for free :peach: :underage:  https://discord.gg/tiktokhomes @everyone
+    //".to_string();
 
-This release was an absolutely monumental amount of work - equivalent to the initial release of Dioxus five times over. Across all repositories we modified over 200,000 lines of code.
+    //    let input = "
+    //    @everyone @here
+    //            🔥 Burn 0.14.0 release <:EmberAmazed:1184985554014715935> @everyone
+    //
+    //This release marks the debut of our CubeCL integration, which brings cross-platform GPU programming capabilities directly to Rust. With CubeCL now supporting both CUDA and WebGPU, Burn benefits from a new CUDA backend that can be enabled using the `cuda-jit` feature. Please note that this backend is still considered experimental, and some operations, particularly those related to vision, may experience issues.
+    //
+    //Burn 0.14.0 introduces a new tensor data format that significantly enhances serialization and deserialization speeds and introduces Quantization, a new Beta feature included in this release. The format is not compatible with previous versions of Burn, but you can migrate your previously saved records using [this guide](https://github.com/tracel-ai/burn?tab=readme-ov-file#deprecation).
+    //
+    //Thanks to all contributors, over 50 for this release alone 🎉
+    //
+    //There's a lot more in this release! I recommend taking a look at the full release notes.
+    //
+    //Release Notes: https://github.com/tracel-ai/burn/releases/tag/v0.14.0
+    //Reddit Post: https://www.reddit.com/r/rust/comments/1f2n1iq/burn_0140_released_the_first_fully_rustnative/
+    //            ".to_string();
 
-With the signal rewrite, Dioxus is much much easier to work with. Between cargo templates, hotreloading, autoformatting, Copy-state, and the bundler, it's hard to express how quickly you can get an app off the ground. I encourage everyone to give the new version for a test drive.
-
-The stuff that's changed includes:
-
-- Complete rewrite of dioxus-core, removing all unsafe code
-- Abandoning use_state and use_ref for a clone-free Signal-based API
-- Removal of all lifetimes and the cx: Scope state
-- A single, unified launch function that starts your app for any platform
-- Asset hotreloading that supports Tailwind and Vanilla CSS
-- Rewrite of events, allowing access to the native WebSys event types
-- Extension of components with element properties (IE a Link now takes all of <a/> properties)
-- Integrated Error Boundaries and Server Futures with Suspense integration
-- 5x faster desktop reconciliation and custom asset handlers for streaming bytes
-- Streaming server functions and fullstack hotreloading
-- Tons of QoL improvements, bug fixes, and more!
-- I want to call out the small but mighty Dioxus core team: ealmloff, dogedark, and nicoburns, as well as marc2332.
-
-Thanks for the all the support, we're excited for the rest of 2024.
-        ".to_string();
-    //let input =
-    //    "come here aaaaaaaaaaaaaaaaaaaaaa https://discord.gg/sexx @everyone @here".to_string();
+    let input =
+        "come here aaaaaaaaaaaaaaaaaaaaaa https://discord.gg/sexx @everyone @here".to_string();
     //let input = "@everyone ny64 is furry".to_string();
     let test = Array2::from_shape_vec((1, MAX_LEN), convert(input)).unwrap();
     println!(
@@ -83,11 +73,16 @@ struct Data {
 
 fn convert(message: String) -> Vec<f32> {
     let spam_words: [(&str, f32); MAX_LEN] = [
+        ("now", 1.0),
+        ("announced", 1.0),
+        ("release", 1.0),
         ("$", 0.8),
         ("nsfw", 2.0),
-        ("who", 2.0),
-        ("want", 2.0),
+        ("who", 1.2),
+        ("want", 1.2),
+        ("join", 1.0),
         ("bro", 1.0),
+        ("you", 1.0),
         ("teen", 0.8),
         ("onlyfans", 2.0),
         ("leaks", 2.0),
@@ -101,17 +96,21 @@ fn convert(message: String) -> Vec<f32> {
         ("(", 0.8),
         (")", 0.8),
         ("sex", 2.0),
-        ("@everyone", 0.2),
+        ("@everyone", 0.8),
         ("@here", 0.4),
     ];
     let mut res = vec![];
     for spam_word in spam_words {
-        res.push((have_cnt(&message, spam_word.0) as f32).powf(spam_word.1))
+        res.push((have_cnt(&message, spam_word.0) as f32 + 0.1).powf(spam_word.1))
     }
+    println!("{res:?}");
     res
 }
 
 fn have_cnt(text: &str, c: &str) -> usize {
+    let mut message = "_".to_string();
+    message.push_str(text);
+    message.push('_');
     let text = text
         .replace("money", "$")
         .replace(":peach:", "🍑")
